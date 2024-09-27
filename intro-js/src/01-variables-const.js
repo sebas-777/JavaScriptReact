60d2e8f1629bb44e1500a7f7fc1c445d76a1504d
sebas-777 <sebastiancast06@hotmail.com>
2024-09-27 05:18:02 -0500

    first commit

---
+++ intro-js/src/01-variables-const.js
@@ -0,0 +1,14 @@
+
+let firstName = 'pepe ';
+firstName = ' andres';
+
+const lastName = 'Doe';
+
+const condicion = true;
+
+if (condicion) {
+    const lastName  = 'Alex';
+    console.log(lastName);
+}
+
+console.log(`Hello, World! ${firstName} ${lastName}`);
